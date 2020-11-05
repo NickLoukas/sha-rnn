b0c94934d774fba07edc8733f292bd5c68fb5cf9
Nick Loukas <nickloukas@outlook.com>
2020-11-05 12:07:15 +0200

    Errors

--- model.py
+++ model.py
@@ -111,7 +111,7 @@ class Attention(nn.Module):
                 cogn_embs = cogn_embs.transpose(1,0)
                 cogn_embs = self.cogn(cogn_embs)
                 cogn_embs = cogn_embs.unsqueeze(1)
-                target = torch.zeros(query.shape[0], query.shape[1], query.shape[2])
+                target = torch.zeros(query.shape[0], query.shape[1], query.shape[2]).cuda()
                 target[:cogn_embs.shape[0], :cogn_embs.shape[1], :cogn_embs.shape[2]] = cogn_embs
                 query = self.q(target)
             else:
